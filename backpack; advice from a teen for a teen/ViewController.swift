7be297136b660f8944f38c70d0fad326b9f241f1
Christine <lee.christine521@gmail.com>
2020-06-09 13:58:55 -0400

    Initial Commit

---
+++ backpack; advice from a teen for a teen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  backpack; advice from a teen for a teen
+//
+//  Created by Christine Lee on 6/9/20.
+//  Copyright © 2020 Christine Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ backpack; advice from a teen for a teenTests/backpack__advice_from_a_teen_for_a_teenTests.swift
@@ -0,0 +1,34 @@
+//
+//  backpack__advice_from_a_teen_for_a_teenTests.swift
+//  backpack; advice from a teen for a teenTests
+//
+//  Created by Christine Lee on 6/9/20.
+//  Copyright © 2020 Christine Lee. All rights reserved.
+//
+
+import XCTest
+@testable import backpack__advice_from_a_teen_for_a_teen
+
+class backpack__advice_from_a_teen_for_a_teenTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
